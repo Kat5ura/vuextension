84289d17b5dfbad8f2b259fa8161c2705af84155
kat5ura <971969499@qq.com>
2018-10-30 18:48:13 +0800

    增加 Element-ui

--- src/pages/options.js
+++ src/pages/options.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
-import Main from './options/options.vue';
+import Options from './options/options.vue';
 
+Vue.use(ElementUI);
 
 new Vue({
     el: '#app',
-    render: r => r(Main)
+    render: r => r(Options)
 });
